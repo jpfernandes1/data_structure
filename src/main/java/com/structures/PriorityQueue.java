c212c35ce1aa8adfd32fbf5197a20ea2deee1649
jpfernandes1 <jp_fernandes1@live.com>
2026-01-09 14:40:12 -0300

    feat: add class to generate queues ordered by priority.

---
+++ src/main/java/com/structures/PriorityQueue.java
@@ -0,0 +1,31 @@
+package com.structures;
+
+public class PriorityQueue<T> extends Queue<T> {
+
+    /*
+        To prioritize a queue, only the add method must be different.
+     */
+
+    @Override
+    public boolean add(T element){
+
+        Comparable<T> key = (Comparable<T>) element;
+        int i;
+        T[] allElements = getAllElements();
+
+        for (i=0; i<size(); i++){
+            if(key.compareTo(allElements[i]) < 0){
+                break;
+            }
+        }
+
+        if (size() == 0 || i == size()) {
+            // Adds the element to the end if the queue is empty or element has lower priority
+            return super.add(element);
+        } else {
+
+        return super.add(i, element);
+        }
+    }
+
+}
